7112edfba646447c9a715934f0c7f6844dec17d4
Anita <nitabunni@hotmail.com>
2014-12-25 15:04:12 -0500

    logic1.py

--- logic1.py
+++ logic1.py
@@ -59,8 +59,67 @@ Unless it is your birthday -- on that day, your speed can be 5 higher in all cas
         
     elif speed>=81:
         return 2
+        
+def sorta_sum(a, b):
+'''
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20. 
+''' 
+    if (a+b>=10) and (a+b<=19):
+        return 20
+    else:
+        return a+b
+    
+def alarm_clock(day, vacation):
+'''
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring.
+Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off". 
+'''
+    while vacation:
+        if day==0 or day==6:
+            return "off"
+        elif day>=1 or day<=5:
+            return "10:00"
+            
+    if (day==0 or day==6) and (vacation==False):
+            return "10:00"
+            
+    elif (day>=1 or day<=5) and (vacation==False):
+        return "7:00"
+        
+def love6(a, b):
+'''
+The number 6 is a truly great number. Given two int values, a and b, return True if either one is 6. 
+Or if their sum or difference is 6. Note: the function abs(num) computes the absolute value of a numb
+'''
+    if (a==6) or (b==6):
+        return True
+    elif (a+b==6) or (a-b==6) or (b-a==6):
+        return True
+    else:
+        return False
   
-       
+def in1to10(n, outside_mode):
+'''
+Given a number n, return True if n is in the range 1..10, inclusive. Unless "outsideMode" is True, in which case return True if the number is less or equal to 1, or greater or equal to 10. 
+'''
+    if (n<=1 or n>=10) and (outside_mode):
+        return True
+    elif (n>=1 and n<=10) and (outside_mode==False):
+        return True
+    else:
+        return False
+    
+        
+def near_ten(num):
+'''
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. 
+'''
+
+    if (num % 10 <= 2) or (num % 10 >=8 and num % 10 <10):
+        return True
+    else:
+        return False
+         
   
         
   
